052eaff33130004e1f2355a853fcbd0feee55cbc
Bhaveshh-15 <naikbhaves@gmail.com>
2023-12-02 17:19:22 +0530

    Updated JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,31 @@
-let inp;
-let city;
-document.getElementById("submit").onclick=function(){
-        
-    inp=document.getElementById("myinp").value;
-    if(inp==""){
-        window.alert("Please Enter a Value");
+
+
+var searchbut=document.getElementById("submit");
+searchbut.addEventListener('click',function(e){
+    var cityinp=document.getElementById("myinp");
+    var city=cityinp.value;
+    if(cityinp==""){
+        window.alert("PLEASE ENTER CITY");
     }
-    document.getElementById("opcity").innerHTML="Showing Result of Weather for the City :"+ inp;
-        
+    else{
+
+        checkWeather();
+        cityinp.value="";
+    }
+    
+})
+
+
+const apikey="42a36296bc65f610a23d0bbe9a176983";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=56d5c8908be366ba059962f0e2bf8a1c";
+
+    async function checkWeather(){
+        const response=await fetch(apiUrl+'&appid=${apikey}');
+        var data=await response.json();
+
+        //console.log(data)
+        document.querySelector(".cityname").innerHTML=data.name;
+        document.querySelector(".Temperature").innerHTML=data.main.temp;
+        document.querySelector(".humidity").innerHTML=data.main.humidity;
+        document.querySelector(".wind").innerHTML=data.main.wind;
 }
